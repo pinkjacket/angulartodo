e42eadef6df61b6346929028c36c1bced0b9e729
James Donlan <jacketlizard@live.com>
2018-02-19 15:47:26 -0800

    angular project set up, attempting to use webpack rather than gulp

---
+++ webpack.test.js
@@ -0,0 +1,9 @@
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+	target: 'node',
+	externals: [nodeExternals() ],
+};
